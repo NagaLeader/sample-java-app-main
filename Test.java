8a39301b64625b03983ada94b7316d0c3cee3072
NagaLeader <mailmesatyajava@rediffmail.com>
2022-01-14 21:26:41 -0800

    adding the dev

---
+++ Test.java
@@ -0,0 +1,13 @@
+package com.itc;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Employee employee = new Employee();
+		employee.setId("2");
+		System.out.println("Hello World");
+
+	}
+
+}
