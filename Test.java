0d4f1bbd9605cbad19848552f05625893597f872
NagaLeader <mailmesatyajava@rediffmail.com>
2022-01-15 04:56:54 -0800

    adding the feature changes

--- Test.java
+++ Test.java
@@ -7,12 +7,10 @@ public class Test {
 		Employee employee = new Employee();
 		employee.setId("2");
 		System.out.println("Hello World");
-
-		System.out.println("employee"+employee.getId());
-	
+		System.out.println(employee.getId());
 		System.out.println(employee.getName());
 		System.out.println("Hello World");
-
+		System.out.println("Git Revert");
 
 	}
 
