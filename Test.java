1a2ec7fe2e81ffc8a9a406991d1b5edb717f18b8
NagaLeader <mailmesatyajava@rediffmail.com>
2022-01-15 05:21:19 -0800

    adding the revert

--- Test.java
+++ Test.java
@@ -7,11 +7,17 @@ public class Test {
 		Employee employee = new Employee();
 		employee.setId("2");
 		System.out.println("Hello World");
-		System.out.println(employee.getId());
+
+		System.out.println("employee"+employee.getId());
+	
 		System.out.println(employee.getName());
 		System.out.println("Hello World");
+<<<<<<< HEAD
 		System.out.println("Git Revert");
 		System.out.println("Git Revert1");
+=======
+
+>>>>>>> parent of 0d4f1bb (adding the feature changes)
 
 	}
 
